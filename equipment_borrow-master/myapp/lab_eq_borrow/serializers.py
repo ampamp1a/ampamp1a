fd10939d28dac4d294aa54bcaf9c8842c6dcdf71
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2023-03-09 23:57:19 +0700

    add project mysql

---
+++ equipment_borrow-master/myapp/lab_eq_borrow/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from .models import *
+
+class FacultiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Facultie
+        fields = ('f_id','f_name')
+
+class ItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('item_id','item_id_type','item_name','item_category',
+                  'item_description','item_faculty','item_department',
+                  'item_status','item_created_at','item_updated_at')
+        
+class UserPrivilegeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_privilege
+        fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class IdTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Id_type
+        fields = '__all__'
+
+
+class ItemCategorieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item_categorie
+        fields = '__all__'
+
+
+class ItemStatuseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item_statuse
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class BorrowInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrow_info
+        fields = '__all__'
